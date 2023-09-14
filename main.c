b2211b34c44a6b860d2f2a29b316a433281f6dcb
ranicocs <danielqueijo14@gmail.com>
2023-09-14 21:23:27 +0200

    Add tokenization

--- main.c
+++ main.c
@@ -1,20 +1,33 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
 
-int chopString(char * cadena, char * trozos[]);
+#define MAX 5
+#define BUFFER_SIZE 1024
+
+int chopCmd(char * cmd, char * tokens[]);
 void printPrompt ();
-void readInputs();
-void processCommand();
+void readInputs(char *cmd, char *arguments[], int *nArguments);
+bool processCommand();
+
+void cmdAuthors ();
 
 int main() {
-    bool quit = true;
+    bool quit;
 
-    while (quit) {
-        printPromp();
-        readInputs();
+    char *cmd;
+    char *arguments[MAX];
+    int nArguments;
 
-    }
+
+    do {
+        printPrompt();
+        readInputs(cmd, arguments, &nArguments);
+        quit = processCommand(arguments);
+
+    } while (quit);
 
 
     return 0;
@@ -25,24 +38,54 @@ void printPrompt () {
     printf(">> ");
 }
 
-void readInputs() {
-    char *cmd;
-    char *arguments[5];
-    int nArguments;
+void readInputs(char *cmd, char *arguments[], int *nArguments) {
+    int i = 0;
 
-    fgets(cmd, 60, stdin);
-    nArguments = chopString(cmd, arguments);
+    cmd = malloc(sizeof(char) * BUFFER_SIZE);
+    fgets(cmd, BUFFER_SIZE, stdin);
+    // printf("%s\n", cmd);
 
 
+    *nArguments = chopCmd(cmd, arguments);
+    // while (i < *nArguments) {
+    //         printf("%s\n", arguments[i]);
+    //         i++;
+    // }
+    // Tokeniza ben
+
+    free(cmd);
 }
 
-int chopString(char * cadena, char * trozos[]) {
+bool processCommand(char *arguments[MAX]) {
+
+    if (strcmp(arguments[0], "authors") == 0) cmdAuthors();
+    // switch () {
+    //     case 'authors':
+
+    //         break;
+    //     case 'date':
+
+    //         break;
+    //     case 'time':
+
+    //         break;
+
+    //     default:
+    //         break;
+    // }
+    return false;
+}
+
+int chopCmd(char * cmd, char * tokens[]) {
     int i=1;
-    if ((trozos[0]=strtok(cadena," \n\t"))==NULL)
+    if ((tokens[0]=strtok(cmd," \n\t"))==NULL)
         return 0;
-    while ((trozos[i]=strtok(NULL," \n\t"))!=NULL)
+    while ((tokens[i]=strtok(NULL," \n\t"))!=NULL)
         i++;
     return i;
 }
 
 
+void cmdAuthors () {
+    printf("iago dani");
+}
